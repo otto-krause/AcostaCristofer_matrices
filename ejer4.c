4c8ef43bb36752f4fd54b29c50be64984e3b08c1
CristoferAcosta <86359491+CristoferAcosta@users.noreply.github.com>
2021-11-24 01:11:43 -0300

    Add files via upload

---
+++ ejer4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0, j=0, m[4][13], total = 0, lineas = 0;
+    for(i=1;i<4;i++){
+        for(j=1;j<13;j++){
+            printf("Ingrese cuanto recaudo el auto %d linea %d : ",j,i);
+            scanf("%d",&m[i][j]);
+        }
+    }
+    for(i=1;i<4;i++){
+        for(j=1;j<13;j++){
+            total+=m[i][j];
+        }
+    }
+    for(i=1;i<4;i++){
+        lineas=0;
+        for(j=1;j<13;j++){
+                lineas+=m[i][j];
+        }
+        printf("Recaudacion de la linea %d es : %d \n",i,lineas);
+    }
+    printf("La recaudacion total es : %d ",total);
+}
